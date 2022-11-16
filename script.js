3fd0168998d0239e63ae3c7e6d76c268e9a1bf71
khasanovmma <khasanovmma010@gmail.com>
2022-11-16 18:54:49 +0500

    js1

---
+++ script.js
@@ -0,0 +1,16 @@
+var siznig_ismingiz  = prompt("ismingizni kiriting");
+console.log("siznig ismingiz: "+siznig_ismingiz)
+var siznig_yoshigiz  = prompt("yoshingizni kiriting");
+console.log("siznig yoshigiz: "+siznig_yoshigiz)
+var misol = +prompt("4+4=?")
+console.log("misol 4+4=8: sizning javobingiz: "+misol)
+var misol = +prompt("8-4=?")
+console.log("misol 8-4=4: sizning javobingiz: "+misol)
+var misol = +prompt("4*4=?")
+console.log("misol 4*4=16: sizning javobingiz: "+misol)
+var misol = +prompt("8/4=?")
+console.log("misol 8/4=2: sizning javobingiz: "+misol)
+var misol = +prompt("13%1=?")
+console.log("misol 13%3=1: sizning javobingiz: "+misol)
+var natija =alert("natijani korish uchun consolni oching")
+console.log(console)
